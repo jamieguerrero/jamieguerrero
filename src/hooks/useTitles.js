dfc311fb27010b04d2d3bdc0464462dcdfe32d55
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2022-01-30 00:26:47 -0500

    add cleanup to useTitle hook

--- src/hooks/useTitles.js
+++ src/hooks/useTitles.js
@@ -4,7 +4,11 @@ export default function useTitles() {
   const titles = ["Hi, I'm Jamie Guerrero ✌🏼", "It's nice to meet you!"];
   const [titleIndex, setTitleIndex] = useState(0);
   useEffect(() => {
-    setTimeout(() => setTitleIndex((i) => (i === 0 ? 1 : 0)), 1500);
+    const id = window.setTimeout(
+      () => setTitleIndex((i) => (i === 0 ? 1 : 0)),
+      1500
+    );
+    () => window.clearTimeout(id);
   }, [titleIndex]);
 
   return { title: titles[titleIndex] };
